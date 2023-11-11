f73a821c5aa1a37b90e0d5deb893729c91034158
GabrielAcosta <gabriacos@gmail.com>
2023-11-11 20:23:48 -0300

    Se modifco la base de dastos de Alumnos

--- Alumnos/models.py
+++ Alumnos/models.py
@@ -7,5 +7,6 @@ class Alumno(models.Model):
     DNI = models.CharField(max_length=9, primary_key=True)
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
-    email = models.EmailField()
-    fecha_nacimiento = models.DateField()
+    email = models.EmailField( blank=True, null=True, default='No asignado')
+    telefono= models.CharField(max_length=9, blank=True, null=True, default='No asignado')
+    curso = models.CharField(max_length=255, blank=True, null=True, default='No asignado')
