195011092ab856c98bb834eab82b4d64b4a43744
GabrielAcosta <gabriacos@gmail.com>
2023-11-11 22:03:22 -0300

    Se creó el metodo POST para crear usuarios, se agregó el formulario para la agregación de usuarios

--- Alumnos/views.py
+++ Alumnos/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Alumno
 # Create your views here.
 def home(request):
     Alumnos = Alumno.objects.all()
     return render(request, 'gestionAlumnos.html',{'Alumnos':Alumnos})
+
+def registrarAlumno(request):
+    nombre = request.POST['nombre']
+    Apellidos = request.POST['Apellidos']
+    DNI = request.POST['DNI']
+    telefono = request.POST['telefono']
+    email = request.POST['email']
+    curso = request.POST['curso']
+    nuevoAlumno = Alumno.objects.create(
+        DNI = DNI,
+        nombre = nombre,
+        apellidos = Apellidos,
+        telefono=telefono,
+        email=email,
+        curso=curso
+    )
+    return redirect('/')
